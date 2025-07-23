e42c08bb117ded0fcba9411925354813ff081b20
mrdatcan <datcanlmht@gmail.com>
2025-07-23 19:45:00 +0700

    alo

---
+++ fe/sivirshop/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Homepage from './pages/users/homepage';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Homepage />);
+
---
+++ fe/sivirshop/src/pages/users/homepage/index.js
@@ -0,0 +1,18 @@
+import {memo} from 'react';
+const HomePage = () => {
+return 
+    <>
+    <h1>Header</h1>
+    <h1>vody</h1>
+    <h1>footer</h1>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home page of our application.</p>
+        <p>This is the main content of the home page.</p>
+        <p>Feel free to explore our features and offerings.</p>
+        <p>Stay tuned for updates and new content!</p>
+        <p>Thank you for visiting our site.</p>
+        <p>We hope you have a great experience.</p>     
+    </>;
+};
+
+export default memo(HomePage);
